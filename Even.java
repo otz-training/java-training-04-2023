bf9855a2d1556c4d1df15dcc63e980dcfb8859f5
otz-training <opentechztraining123@gmail.com>
2023-03-30 18:50:39 +0530

    30th March Class uploded

---
+++ Even.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+class Even
+{
+    public static void main(String[] args)
+    {
+        int num;
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter a number :");
+         num = sc.nextInt();
+         if(num%2==0)
+         {
+              System.out.println("Number is Even");
+         }else{
+
+              System.out.println("Number is Odd");
+         }
+    }
+}
